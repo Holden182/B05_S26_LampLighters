d2a2c285b6099b52ec1c4a080abbf73c05318db4
stephaniekim425-pixel <stephaniekim425@gmail.com>
2026-02-24 17:35:54 -0500

    Character Animation
    walks when moving, idle animation when standing still

---
+++ LampLighters/Assets/scripts/PlayerAnimation.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerAnimationDriver : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 horizVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        float speed = horizVel.magnitude;
+
+        bool isWalking = speed > 0.1f;
+        anim.SetBool("walk", isWalking);
+    }
+}
