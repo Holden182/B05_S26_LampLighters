60e71a74e91d08d6a5ae5b5ac95d7f525b7dbdc4
stephaniekim425-pixel <stephaniekim425@gmail.com>
2026-03-03 01:00:57 -0500

    InstructionsPanelAdded

---
+++ LampLighters/Assets/scripts/OpenInstructions.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class OpenInstructions : MonoBehaviour
+{
+   public GameObject Panel;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
+   public void OpenPanel()
+   {
+        if(Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+   }
+}
