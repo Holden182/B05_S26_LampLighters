d3174ae058e1baf3801f63746d61294fa7cdfa8a
Quinn22k <qkesse01@tufts.edu>
2026-03-03 11:59:16 -0500

    Textured map on level 1 + adding music!

---
+++ LampLighters/Assets/scripts/AudioManagerGame.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+public class AudioManager : MonoBehaviour
+
+{
+   [Header("-----Audio Source-----")]
+   [SerializeField] AudioSource musicSource;
+
+   [Header("-----Audio Clip-----")]
+   public AudioClip music; 
+
+
+private void Start()
+   {
+      musicSource.clip = music;
+      musicSource.Play();
+   }
+
+}
